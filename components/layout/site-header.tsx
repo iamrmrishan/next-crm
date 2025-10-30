0d3b4df8c6a6ce8962bc15f22972de5275d69689
iamrmrishan <iamrmrishan@gmail.com>
2025-10-30 22:54:39 +0530

    fix: hide git button

--- components/layout/site-header.tsx
+++ components/layout/site-header.tsx
@@ -87,7 +87,7 @@ export function SiteHeader({
         <div className="container flex h-16 items-center">
           <div className="flex flex-1 items-center justify-end space-x-4">
             <nav className="flex items-center space-x-2">
-              <Button asChild size="lg" variant="ghost">
+              {/* <Button asChild size="lg" variant="ghost">
                 <Link
                   href="https://github.com/iamrmrishan/next-crm"
                   target="_blank"
@@ -102,7 +102,7 @@ export function SiteHeader({
                   </svg>
                   <span>Github</span>
                 </Link>
-              </Button>
+              </Button> */}
               <ThemeToggle />
 
               {/* Auth Section */}
